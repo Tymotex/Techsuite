92b529d29a85c7b442ce598f73385683e73fbb06
Tymotex <timzhang3@gmail.com>
2020-06-21 17:50:20 +1000

    boilerplate hacker news API consumption

--- client/src/views/pages/News.js
+++ client/src/views/pages/News.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import axios from 'axios';
+
+// Documentation: https://github.com/HackerNews/API
 
 class News extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount() {
+        console.log("===== HERE =====");
+        axios.get("https://hacker-news.firebaseio.com/v0/item/9129911.json?print=pretty")
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
     render() {
         return (
             <div>
