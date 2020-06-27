c39eca6fcc0017f17d89a7a681f1fd36f1a531f3
Tymotex <timzhang3@gmail.com>
2020-06-28 08:14:35 +1000

    Added sample hackernews fetched links

--- client/src/views/pages/News.js
+++ client/src/views/pages/News.js
@@ -6,12 +6,35 @@ import axios from 'axios';
 class News extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            stories: []
+        }
     }
 
     componentWillMount() {
-        axios.get("https://hacker-news.firebaseio.com/v0/item/9129911.json?print=pretty")
+        axios.get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
             .then((res) => {
-                console.log(res.data);
+                // Top 500 stories:
+                var i = 0;
+                res.data.forEach((elem) => {
+                    i++;
+                    if (i < 10) {
+                        axios.get(`https://hacker-news.firebaseio.com/v0/item/${elem}.json?print=pretty`)
+                            .then((storyRes) => {
+                                this.setState({
+                                    stories: [...this.state.stories, storyRes.data]
+                                });
+                                console.log(this.state);
+                            })
+                            .catch((err) => {
+                                console.log(err);
+                            });
+                    }
+                });
+
+                
+                    
+                // 
             })
             .catch((err) => {
                 console.log(err);
@@ -22,6 +45,11 @@ class News extends React.Component {
         return (
             <div>
                 Hacker news.
+                { this.state.stories.map(story => (
+                    <div>
+                        <a href={story.url}>{story.title}</a>
+                    </div>
+                )) }
             </div>
         );
     }
